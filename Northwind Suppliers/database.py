2a538d1bba229ffc13d9edc477ef922819b2d336
acebk <81196056+acebk@users.noreply.github.com>
2022-01-01 22:31:57 -0500

    initialized functions

--- Northwind Suppliers/database.py
+++ Northwind Suppliers/database.py
@@ -3,6 +3,10 @@ import sqlite3
 def query(query_text):
     conn=sqlite3.connect("Northwind_large.sqlite")
     cur = conn.cursor()
-    cur = execute(query_text)
+    cur.execute(query_text)
+    rows = cur.fetchall()
+    return rows
+    conn.close()
 
-    
+def get_all_facts():
+    return query(""" SELECT * FROM Supplier """)
