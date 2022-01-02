1265abda41aa7d7d18d75709076f826b05186c51
acebk <81196056+acebk@users.noreply.github.com>
2022-01-01 23:32:57 -0500

    Made it so desired rows are taken

--- Northwind Suppliers/database.py
+++ Northwind Suppliers/database.py
@@ -4,9 +4,26 @@ def query(query_text):
     conn=sqlite3.connect("Northwind_large.sqlite")
     cur = conn.cursor()
     cur.execute(query_text)
+    #title = cur.description()
     rows = cur.fetchall()
+    column_names = []
+    #comp_name = []
+    #city = []
+    #country = []
+    for col in cur.description:
+        column_names.append(col[0])
+    dicts = []
+    #for row in rows:
+        #column_names.append(row[0])
+    for row in rows:
+        r = [row[1], row[5], row[8]]
+        d = dict(zip(column_names, r))
+        dicts.append(d)
+        #comp_name.append(row[1])
+        #city.append(row[5])
+        #country.append(row[8])
     conn.close()
-    return rows
+    return dicts
 
 def get_all_facts():
     return query(""" SELECT * FROM Supplier """)
